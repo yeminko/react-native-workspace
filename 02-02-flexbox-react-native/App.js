e32c40038d88ee29e31c06fec8b3489b131dac5b
yeminko <yeminko47@gmail.com>
2025-10-10 09:15:53 +0630

    Add package.json for 02-02-flexbox-react-native project with initial dependencies and scripts

---
+++ 02-02-flexbox-react-native/App.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        padding: 50,
+        flexDirection: "row",
+        width: "80%",
+        height: 300,
+        justifyContent: "space-between",
+        alignItems: "stretch",
+      }}
+    >
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "red",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>1</Text>
+      </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "blue",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>2</Text>
+      </View>
+      <View
+        style={{
+          backgroundColor: "green",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Text>3</Text>
+      </View>
+    </View>
+  );
+}
