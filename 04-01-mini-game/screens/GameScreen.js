d51e3735b47740a6b5d919fe470cdc8f69abbd2d
yeminko <yeminko47@gmail.com>
2025-11-13 09:09:35 +0630

    Implement game logic and input validation; update StartGameScreen and GameScreen components, and refactor PrimaryButton for event handling

---
+++ 04-01-mini-game/screens/GameScreen.js
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function GameScreen() {
+  return (
+    <View style={styles.screen}>
+      <Text>Opponent's Guess</Text>
+      {/* GUESS */}
+      <View>
+        <Text>Higher or Lower?</Text>
+        {/* + - */}
+      </View>
+      {/* <View>LOG ROUNDS</View> */}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 24,
+  },
+});
