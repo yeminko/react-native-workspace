c9fca687ede73a730c875313e8fe18c076074c8c
yeminko <yeminko47@gmail.com>
2025-12-15 09:14:35 +0630

    Add initial setup for meals app with CategoriesScreen
    - Created package.json for project dependencies and scripts.
    - Implemented CategoriesScreen with FlatList to display categories using CategoryGridTile.

---
+++ 06-01-meals-app/components/CategoryGridTile.js
@@ -0,0 +1,32 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable style={styles.button}>
+        <View style={styles.innerContainer}>
+          <Text>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    borderRadius: 8,
+    elevation: 4,
+  },
+  button: {
+    flex: 1,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
