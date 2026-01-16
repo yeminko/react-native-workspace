47e2ec713cc9f321d00a9a08701544f1ab825fd8
yeminko <yeminko47@gmail.com>
2026-01-16 10:10:19 +0630

    Enhance navigation drawer with custom styles and icons; update UserScreen to include drawer toggle functionality

--- 06-02-other-navigators/App.js
+++ 06-02-other-navigators/App.js
@@ -3,15 +3,43 @@ import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
 import WelcomeScreen from "./screens/WelcomeScreen";
 import UserScreen from "./screens/UserScreen";
+import { Ionicons } from "@expo/vector-icons";
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator>
-        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
-        <Drawer.Screen name="User" component={UserScreen} />
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#3498db" },
+          headerTintColor: "#fff",
+          drawerActiveBackgroundColor: "#f0e1ff",
+          drawerActiveTintColor: "#6a0dad",
+          drawerStyle: {
+            backgroundColor: "#e6e6fa",
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{
+            drawerLabel: "Welcome Screen",
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Drawer.Screen
+          name="User"
+          component={UserScreen}
+          options={{
+            drawerIcon: ({ color, size }) => (
+              <Ionicons name="person" size={size} color={color} />
+            ),
+          }}
+        />
       </Drawer.Navigator>
     </NavigationContainer>
   );
