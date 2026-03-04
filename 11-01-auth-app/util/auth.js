044fb515eef7a180a5418bae3999c27d06488e23
yeminko <yeminko47@gmail.com>
2026-03-04 08:51:45 +0630

    Add authentication screens and utility functions
    - Created package.json for the authentication app with necessary dependencies.
    - Implemented LoginScreen and SignupScreen components for user authentication.
    - Added WelcomeScreen to display a success message upon authentication.
    - Developed auth.js utility to handle user creation with Firebase.

---
+++ 11-01-auth-app/util/auth.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+import { FIREBASE_API_KEY } from "@env";
+
+const API_KEY = FIREBASE_API_KEY;
+
+async function createUser(email, password) {
+  const response = await axios.post(
+    `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+    {
+      email: email,
+      password: password,
+      returnSecureToken: true,
+    },
+  );
+}
