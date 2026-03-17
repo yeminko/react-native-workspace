d5bc5a030c997d2a15277ef0ca59cbc200c58ff7
yeminko <yeminko47@gmail.com>
2026-03-17 08:50:57 +0630

    Add initial project setup and screens for native features
    - Created package.json for project configuration with dependencies.
    - Added AllPlaces screen to display a list of places.
    - Created placeholder files for AddPlace, Map, and PlaceDetails screens.

---
+++ 12-01-native-features/components/Places/PlaceItem.js
@@ -0,0 +1,16 @@
+import { Image, Pressable, StyleSheet, View } from "react-native";
+
+export default function PlaceItem({ place, onSelect }) {
+  return (
+    <Pressable onPress={onSelect}>
+      <Image source={{ uri: place.imageUri }} />
+
+      <View>
+        <Text>{place.title}</Text>
+        <Text>{place.address}</Text>
+      </View>
+    </Pressable>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ 12-01-native-features/components/Places/PlacesList.js
@@ -0,0 +1,31 @@
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import PlaceItem from "./PlaceItem";
+
+export default function PlacesList({ places }) {
+  if (!places || places.length === 0) {
+    return (
+      <View style={styles.fallbackContainer}>
+        <Text style={styles.fallbackText}>No places added yet.</Text>
+      </View>
+    );
+  }
+
+  return (
+    <FlatList
+      data={places}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => <PlaceItem place={item} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  fallbackContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  fallbackText: {
+    fontSize: 16,
+  },
+});
